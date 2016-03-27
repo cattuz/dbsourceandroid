cde4758cbab041406ebfac7b015a28f151897d3c
Johan Bergström <johan.carl.bergstrom@gmail.com>
2016-03-27 19:15:59 +0200

    Make default accessors throw database exceptions instead of null pointer exceptions

--- app/src/main/java/com/devexed/dalwitandroid/DefaultAndroidSQLiteAccessorFactory.java
+++ app/src/main/java/com/devexed/dalwitandroid/DefaultAndroidSQLiteAccessorFactory.java
@@ -2,7 +2,6 @@ package com.devexed.dalwitandroid;
 
 import android.database.Cursor;
 import android.database.SQLException;
-
 import com.devexed.dalwit.AccessorFactory;
 import com.devexed.dalwit.DatabaseException;
 
@@ -29,98 +28,98 @@ public final class DefaultAndroidSQLiteAccessorFactory implements
         put(Boolean.TYPE, new AndroidSQLiteAccessor() {
             @Override
             public void set(SQLiteBindable bindable, Integer index, Object value) throws SQLException {
-                if (value == null) throw new NullPointerException("Parameter with type boolean can not be null.");
+                if (value == null) throw new DatabaseException("Parameter with type boolean can not be null.");
                 bindable.bindLong(index, (boolean) (Boolean) value ? 1 : 0);
             }
 
             @Override
             public Object get(Cursor cursor, Integer index) throws SQLException {
                 if (cursor.isNull(index))
-                    throw new NullPointerException("Illegal null value for type boolean in result set.");
+                    throw new DatabaseException("Illegal null value for type boolean in result set.");
                 return cursor.getLong(index) != 0;
             }
         });
         put(Byte.TYPE, new AndroidSQLiteAccessor() {
             @Override
             public void set(SQLiteBindable bindable, Integer index, Object value) throws SQLException {
-                if (value == null) throw new NullPointerException("Parameter with type byte can not be null.");
+                if (value == null) throw new DatabaseException("Parameter with type byte can not be null.");
                 bindable.bindLong(index, (Byte) value);
             }
 
             @Override
             public Object get(Cursor cursor, Integer index) throws SQLException {
                 if (cursor.isNull(index))
-                    throw new NullPointerException("Illegal null value for type byte in result set.");
+                    throw new DatabaseException("Illegal null value for type byte in result set.");
                 return (byte) cursor.getLong(index);
             }
         });
         put(Short.TYPE, new AndroidSQLiteAccessor() {
             @Override
             public void set(SQLiteBindable bindable, Integer index, Object value) throws SQLException {
-                if (value == null) throw new NullPointerException("Parameter with type short can not be null.");
+                if (value == null) throw new DatabaseException("Parameter with type short can not be null.");
                 bindable.bindLong(index, (Short) value);
             }
 
             @Override
             public Object get(Cursor cursor, Integer index) throws SQLException {
                 if (cursor.isNull(index))
-                    throw new NullPointerException("Illegal null value for type short in result set.");
+                    throw new DatabaseException("Illegal null value for type short in result set.");
                 return (short) cursor.getLong(index);
             }
         });
         put(Integer.TYPE, new AndroidSQLiteAccessor() {
             @Override
             public void set(SQLiteBindable bindable, Integer index, Object value) throws SQLException {
-                if (value == null) throw new NullPointerException("Parameter with type int can not be null.");
+                if (value == null) throw new DatabaseException("Parameter with type int can not be null.");
                 bindable.bindLong(index, (Integer) value);
             }
 
             @Override
             public Object get(Cursor cursor, Integer index) throws SQLException {
                 if (cursor.isNull(index))
-                    throw new NullPointerException("Illegal null value for type int in result set.");
+                    throw new DatabaseException("Illegal null value for type int in result set.");
                 return (int) cursor.getLong(index);
             }
         });
         put(Long.TYPE, new AndroidSQLiteAccessor() {
             @Override
             public void set(SQLiteBindable bindable, Integer index, Object value) throws SQLException {
-                if (value == null) throw new NullPointerException("Parameter with type long can not be null.");
+                if (value == null) throw new DatabaseException("Parameter with type long can not be null.");
                 bindable.bindLong(index, (Long) value);
             }
 
             @Override
             public Object get(Cursor cursor, Integer index) throws SQLException {
                 if (cursor.isNull(index))
-                    throw new NullPointerException("Illegal null value for type long in result set.");
+                    throw new DatabaseException("Illegal null value for type long in result set.");
                 return cursor.getLong(index);
             }
         });
         put(Float.TYPE, new AndroidSQLiteAccessor() {
             @Override
             public void set(SQLiteBindable bindable, Integer index, Object value) throws SQLException {
-                if (value == null) throw new NullPointerException("Parameter with type float can not be null.");
+                if (value == null) throw new DatabaseException("Parameter with type float can not be null.");
                 bindable.bindDouble(index, (Float) value);
             }
 
             @Override
             public Object get(Cursor cursor, Integer index) throws SQLException {
                 if (cursor.isNull(index))
-                    throw new NullPointerException("Illegal null value for type float in result set.");
+                    throw new DatabaseException("Illegal null value for type float in result set.");
                 return (float) cursor.getDouble(index);
             }
         });
         put(Double.TYPE, new AndroidSQLiteAccessor() {
             @Override
             public void set(SQLiteBindable bindable, Integer index, Object value) throws SQLException {
-                if (value == null) throw new NullPointerException("Parameter with type double can not be null.");
+                if (value == null) throw new DatabaseException("Parameter with type double can not be null.");
                 bindable.bindDouble(index, (Double) value);
             }
 
             @Override
             public Object get(Cursor cursor, Integer index) throws SQLException {
                 if (cursor.isNull(index))
-                    throw new NullPointerException("Illegal null value for type double in result set.");
+                    throw new DatabaseException("Illegal null value for type double in result set.");
                 return cursor.getDouble(index);
             }
         });
@@ -303,7 +302,7 @@ public final class DefaultAndroidSQLiteAccessorFactory implements
                         os.close();
                     }
                 } catch (IOException e) {
-                    throw new DatabaseException(e);
+                    throw new DatabaseException("IO exception when writing input stream to blob", e);
                 }
 
                 bindable.bindBlob(index, os.toByteArray());
